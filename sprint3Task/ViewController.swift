40da4a909f64f42b19eb288be2865e142ef66cb8
folawer21 <folawer21@mail.ru>
2023-10-16 14:54:51 +0300

    Initial Commit

---
+++ sprint3Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sprint3Task
+//
+//  Created by Александр  Сухинин on 16.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
